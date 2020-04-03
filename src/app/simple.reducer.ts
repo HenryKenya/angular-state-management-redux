5b1128edd9ebfe1e638d00d32468e17bc5261800
HenryKenya <henry.onyango@outlook.com>
2020-04-03 10:45:03 +0300

    handle different actions

--- src/app/simple.reducer.ts
+++ src/app/simple.reducer.ts
@@ -1,3 +1,12 @@
-export function SimpleReducer() {
-    
+import { Action } from '@ngrx/store';
+
+export function SimpleReducer(state: string = "Hello World", action: Action) {
+    switch (action.type) {
+        case 'SPANISH':
+            return state = 'Hola Mundo'
+        case 'FRENCH':
+            return state = 'Bonjour le monde'
+        default:
+            return state
+    }
 }
