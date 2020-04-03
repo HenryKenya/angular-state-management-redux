8830795a9e605ebdb6525ef78283275a5e9844ec
HenryKenya <henry.onyango@outlook.com>
2020-04-03 10:51:08 +0300

    add store to component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+interface AppState {
+  message: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-redux';
+  message$: Observable<string>
+
+  constructor(private store: Store<AppState>) {
+    this.message$ = this.store.select('message');
+  }
+
+  showSpannishMessage() {
+    this.store.dispatch({ type: 'SPANISH' })
+  }
+
+  showFrenchMessage() {
+    this.store.dispatch({ type: 'FRENCH' })
+  }
 }
